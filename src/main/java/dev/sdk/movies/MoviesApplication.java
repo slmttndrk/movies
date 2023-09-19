01c898a21f046d280eaf53a49d283d7253aa140c
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-19 09:46:27 +0200

    Putting annotation that implies this class will be a REST API controller is done.

--- src/main/java/dev/sdk/movies/MoviesApplication.java
+++ src/main/java/dev/sdk/movies/MoviesApplication.java
@@ -2,8 +2,10 @@ package dev.sdk.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
