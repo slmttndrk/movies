464691e846f6dc130cb9976f3f7af13c76bb2e2c
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-19 09:48:44 +0200

    Putting @GetMapping annotation that implies apiRoot method is a GET method

--- src/main/java/dev/sdk/movies/MoviesApplication.java
+++ src/main/java/dev/sdk/movies/MoviesApplication.java
@@ -2,6 +2,7 @@ package dev.sdk.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,4 +13,8 @@ public class MoviesApplication {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
+	@GetMapping
+	public String apiRoot() {
+		return "Hello World!";
+	}
 }
