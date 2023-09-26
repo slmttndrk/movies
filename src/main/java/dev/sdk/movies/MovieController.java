57833ffb9f53df3f9d5265abd0e476e9cb0b8a11
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-26 22:21:28 +0200

    Changed String to ResponseEntity<String> since it is usefull to return status code when working on frontend.

--- src/main/java/dev/sdk/movies/MovieController.java
+++ src/main/java/dev/sdk/movies/MovieController.java
@@ -1,5 +1,7 @@
 package dev.sdk.movies;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/movies")
 public class MovieController {
     @GetMapping
-    public String allMovies() {
-        return "All movies!";
+    public ResponseEntity<String> allMovies() {
+        return new ResponseEntity<String>("All movies!", HttpStatus.OK);
     }
 }
