c9bad2cc7e279e605455989a26fd0d4faf2958b7
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-26 22:44:51 +0200

    Changed ResponseEntity from string to List<Movie> since we want to return list of movies at /api/v1/movies.

--- src/main/java/dev/sdk/movies/MovieController.java
+++ src/main/java/dev/sdk/movies/MovieController.java
@@ -7,13 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
     @Autowired
     private MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> getAllMovies() {
-        return new ResponseEntity<String>("All movies!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 }
