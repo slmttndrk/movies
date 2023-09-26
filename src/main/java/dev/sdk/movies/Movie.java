302cc7d4ff94289b51ce67f4a65272f6bc799502
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-26 21:59:38 +0200

    Added @NoArgsConstructor to create a constructor that takes no parameters as argument.

--- src/main/java/dev/sdk/movies/Movie.java
+++ src/main/java/dev/sdk/movies/Movie.java
@@ -2,6 +2,7 @@ package dev.sdk.movies;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,6 +12,7 @@ import java.util.List;
 @Document(collection = "movies")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
     @Id
     private ObjectId id;
