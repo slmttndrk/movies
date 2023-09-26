7e5b9185e9cff91612c71afe0bc817e9fe642386
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-26 22:12:20 +0200

    To create first Get method, @GetMapping is added.

--- src/main/java/dev/sdk/movies/MovieController.java
+++ src/main/java/dev/sdk/movies/MovieController.java
@@ -1,9 +1,14 @@
 package dev.sdk.movies;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    @GetMapping
+    public String allMovies() {
+        return "All movies!";
+    }
 }
