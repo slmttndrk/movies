973b27441f062e1c47d137acd3dcb354e2e3dcc8
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-26 22:30:30 +0200

    allMovies() method is created to return all the movies.

--- src/main/java/dev/sdk/movies/MovieService.java
+++ src/main/java/dev/sdk/movies/MovieService.java
@@ -2,6 +2,11 @@ package dev.sdk.movies;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MovieService {
+    public List<Movie> allMovies() {
+
+    }
 }
