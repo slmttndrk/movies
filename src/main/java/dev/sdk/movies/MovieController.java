3ea076b911285d6309935fafc545f5408d94f45c
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-26 22:10:55 +0200

    @RestController and @RequestMapping(/api/v1/movies) annotions are added.

--- src/main/java/dev/sdk/movies/MovieController.java
+++ src/main/java/dev/sdk/movies/MovieController.java
@@ -1,4 +1,9 @@
 package dev.sdk.movies;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
 public class MovieController {
 }
