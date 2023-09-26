090ad418c8d50ee77e1d4f4d30aebb68cd290574
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-26 22:40:48 +0200

    In controller, we need a reference to our service class, so movieService is added with @Autowired.

--- src/main/java/dev/sdk/movies/MovieController.java
+++ src/main/java/dev/sdk/movies/MovieController.java
@@ -1,5 +1,6 @@
 package dev.sdk.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
     @GetMapping
     public ResponseEntity<String> getAllMovies() {
         return new ResponseEntity<String>("All movies!", HttpStatus.OK);
