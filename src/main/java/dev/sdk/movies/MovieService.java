fb835a45396c02a8ec437ea966f648c347df7a0d
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-27 11:42:07 +0200

    Since in some cases the method could return null, we explicitly handle it with converting return type from Movie to Optional<Movie>.

--- src/main/java/dev/sdk/movies/MovieService.java
+++ src/main/java/dev/sdk/movies/MovieService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -14,7 +15,7 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
-    public Movie singleMovie(ObjectId id) {
+    public Optional<Movie> singleMovie(ObjectId id) {
         return movieRepository.findById(id);
     }
 }
