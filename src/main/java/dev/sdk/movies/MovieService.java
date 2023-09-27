577aaeb879c321b9ede06f605e163ed9fa5a75df
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-27 11:53:31 +0200

    Changed id parameter to imdbId and findById() to findByImdbId() in MovieService.

--- src/main/java/dev/sdk/movies/MovieService.java
+++ src/main/java/dev/sdk/movies/MovieService.java
@@ -15,7 +15,7 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
-    public Optional<Movie> singleMovie(ObjectId id) {
-        return movieRepository.findById(id);
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
