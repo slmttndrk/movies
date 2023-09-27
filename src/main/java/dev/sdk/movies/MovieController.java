422d9897ad80b87be88649f946e69f615419577c
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-27 11:43:15 +0200

    Since in some cases the method could return null, we explicitly handle it with converting return type from ResponseEntity<Movie> to ResponseEntity<Optional<Movie>>.

--- src/main/java/dev/sdk/movies/MovieController.java
+++ src/main/java/dev/sdk/movies/MovieController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -22,7 +23,7 @@ public class MovieController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Movie> getSingleMovie(@PathVariable ObjectId id) {
-        return new ResponseEntity<Movie>
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id) {
+
     }
 }
