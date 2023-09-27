55a340c0548cb672ef06718d08ef7acfa3dc0c08
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-27 12:30:55 +0200

    Since, when you call insert(), it returns the data you just pushed inside your database.

--- src/main/java/dev/sdk/movies/ReviewService.java
+++ src/main/java/dev/sdk/movies/ReviewService.java
@@ -14,8 +14,7 @@ public class ReviewService {
     @Autowired
     private MongoTemplate mongoTemplate;
     public Review createReview(String reviewBody, String imdbId) {
-        Review review = new Review(reviewBody);
-        reviewRepository.insert(review);
+        Review review = reviewRepository.insert(new Review(reviewBody));
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
