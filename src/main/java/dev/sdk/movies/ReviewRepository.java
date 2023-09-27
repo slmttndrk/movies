45820e401627af7c9615dc780828dbd358d5f626
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-27 12:03:07 +0200

    Added @Repository to ReviewRepository interface.

--- src/main/java/dev/sdk/movies/ReviewRepository.java
+++ src/main/java/dev/sdk/movies/ReviewRepository.java
@@ -2,6 +2,8 @@ package dev.sdk.movies;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
