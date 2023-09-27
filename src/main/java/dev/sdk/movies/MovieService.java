85b6ecda1fa2b057404eb29886d1b76bd45150eb
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-27 11:40:01 +0200

    Created a service method singleMovie(ObjectId id) to get single movie with its specific ID.

--- src/main/java/dev/sdk/movies/MovieService.java
+++ src/main/java/dev/sdk/movies/MovieService.java
@@ -1,5 +1,6 @@
 package dev.sdk.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,5 +12,9 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Movie singleMovie(ObjectId id) {
+        return movieRepository.findById(id);
     }
 }
