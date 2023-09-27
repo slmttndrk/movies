b8b53c5749c89d85b004972c32bec3c0d8286f21
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-27 12:08:06 +0200

    Created createReview(String reviewBody, String imdbId) method to handle creation and connection review with its movie.

--- src/main/java/dev/sdk/movies/ReviewService.java
+++ src/main/java/dev/sdk/movies/ReviewService.java
@@ -4,5 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
-
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = new Review();
+    }
 }
