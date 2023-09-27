5959e0d028c367bf9e7215d2ebd2b80d410180e0
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-27 12:33:00 +0200

    Return the review that is just created.

--- src/main/java/dev/sdk/movies/ReviewService.java
+++ src/main/java/dev/sdk/movies/ReviewService.java
@@ -20,5 +20,7 @@ public class ReviewService {
                 .matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review))
                 .first();
+
+        return review;
     }
 }
