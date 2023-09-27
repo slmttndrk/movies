f034888af5418df3d90576c4171bb91bf33589c4
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-27 11:37:20 +0200

    Created another @GetMapping to get single movie with its specific ID stated in the endpoint using @PathVariable.

--- src/main/java/dev/sdk/movies/MovieController.java
+++ src/main/java/dev/sdk/movies/MovieController.java
@@ -1,9 +1,11 @@
 package dev.sdk.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +19,10 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getSingleMovie(@PathVariable ObjectId id) {
+        return new ResponseEntity<Movie>
     }
 }
