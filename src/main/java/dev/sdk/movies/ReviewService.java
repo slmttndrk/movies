3c7213100387290e9b9e235aa08c1607c465c2fb
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-27 12:15:10 +0200

    A review is associated with ReviewRepository.

--- src/main/java/dev/sdk/movies/ReviewService.java
+++ src/main/java/dev/sdk/movies/ReviewService.java
@@ -1,10 +1,15 @@
 package dev.sdk.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
     public Review createReview(String reviewBody, String imdbId) {
         Review review = new Review(reviewBody);
+
+        reviewRepository.insert(review);
     }
 }
