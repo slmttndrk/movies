02a82b29f8ad574894d1bde7e75dd2c4fc499549
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-28 12:47:29 +0200

    @PostMapping is created with a paramater which it takes the request body and converts it to Map<String, String> named payload.

--- src/main/java/dev/sdk/movies/ReviewController.java
+++ src/main/java/dev/sdk/movies/ReviewController.java
@@ -1,8 +1,13 @@
 package dev.sdk.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -10,5 +15,8 @@ public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
 
+    }
 }
