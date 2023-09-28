c808bc72e35ae27b4aacfb1efa830766b5e278b7
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-28 12:44:15 +0200

    In ReviewController, we first call our service.

--- src/main/java/dev/sdk/movies/ReviewController.java
+++ src/main/java/dev/sdk/movies/ReviewController.java
@@ -1,12 +1,14 @@
 package dev.sdk.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class ReviewController {
-
+    @Autowired
+    private ReviewService reviewService;
 
 
 }
