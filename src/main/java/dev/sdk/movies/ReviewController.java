380ac3b898431437dbe7b205d230d0bfbb0e4662
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-28 12:53:10 +0200

    Changed the endpoint from movies to reviews, since it is more appropriate.

--- src/main/java/dev/sdk/movies/ReviewController.java
+++ src/main/java/dev/sdk/movies/ReviewController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/movies")
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
