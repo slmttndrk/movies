ca65fa450ba0a009a57a14025a251eb0118f0a6d
Selamettin Dirik <selamettinn.95@gmail.com>
2023-09-28 12:40:41 +0200

    In ReviewController, @RequestMapping is added not @GetMapping.

--- src/main/java/dev/sdk/movies/ReviewController.java
+++ src/main/java/dev/sdk/movies/ReviewController.java
@@ -1,7 +1,10 @@
 package dev.sdk.movies;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/movies")
 public class ReviewController {
+
 }
