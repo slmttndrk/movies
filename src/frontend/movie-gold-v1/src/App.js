705f58a1de278c9b2a163428399e7bc3f1293f6a
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-02 11:23:33 +0200

    Returned destructured array and stored an array of movie data.

--- src/frontend/movie-gold-v1/src/App.js
+++ src/frontend/movie-gold-v1/src/App.js
@@ -3,6 +3,9 @@ import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [movies, setMovies] = useState();
+
   return (
     <div className="App">
 
