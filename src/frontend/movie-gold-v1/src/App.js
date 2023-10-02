e17a0e5889ce1c550e1db49910281cb3b7c0058e
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-02 11:30:23 +0200

    Getting movies is handled using async since it can take long time. This makes the UX better. Used try-catch block since different status code can be returned.

--- src/frontend/movie-gold-v1/src/App.js
+++ src/frontend/movie-gold-v1/src/App.js
@@ -6,6 +6,21 @@ function App() {
 
   const [movies, setMovies] = useState();
 
+  const getMovies = async () =>{
+    try 
+    {
+
+      const response = await api.get("/api/v1/movies");
+
+      setMovies(response.data);
+            
+    } 
+    catch (error) 
+    {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
 
