e5a6e881d38bb95856ecf32d5770a5449b539376
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-05 13:43:09 +0200

    Imported carousel anad paper components into Hero component.

--- src/frontend/movie-gold-v1/src/components/hero/Hero.js
+++ src/frontend/movie-gold-v1/src/components/hero/Hero.js
@@ -1,9 +1,11 @@
-import React from "react";
+import './Hero.css';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@mui/material';
 
 const Hero = () => {
     return (
         <div>
-            
+
         </div>
     )
 }
