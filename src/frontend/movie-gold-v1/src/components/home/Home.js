46918a8374ddf8825fa091eb7c6beb4b90704687
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-05 13:54:03 +0200

    Hero component is imported into the Home component.

--- src/frontend/movie-gold-v1/src/components/home/Home.js
+++ src/frontend/movie-gold-v1/src/components/home/Home.js
@@ -1,10 +1,8 @@
-import React from "react";
+import Hero from "../hero/Hero";
 
 const Home = ({movies}) => {
     return (
-        <div>
-            Welcome!!!
-        </div>
+        <Hero movies = {movies} />
     )
 }
 
