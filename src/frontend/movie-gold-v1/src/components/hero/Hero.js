0954ea63315be1a6722a0f99da02fbb4e4c11c59
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-05 13:52:09 +0200

    Hero component is updated with related props.

--- src/frontend/movie-gold-v1/src/components/hero/Hero.js
+++ src/frontend/movie-gold-v1/src/components/hero/Hero.js
@@ -4,9 +4,28 @@ import { Paper } from '@mui/material';
 
 const Hero = ({movies}) => {
     return (
-        <div>
+        <div className='movie-carousel-container'>
             <Carousel>
-                
+            {
+                movies.map((movie) =>{
+                    return(
+                        <Paper>
+                            <div className='movie-card-container'>
+                                <div className='movie-card'>
+                                    <div className='movie-detail'>
+                                        <div className='movie-poster'>
+                                            <img src={movie.poster} alt=""/>
+                                        </div>
+                                        <div className='movie-title'>
+                                            <h4>{movie.title}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </Paper>
+                    )
+                })
+            }
             </Carousel>
         </div>
     )
