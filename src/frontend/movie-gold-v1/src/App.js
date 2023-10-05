fdf3f38fb7baacf104fbfa621f051707c44de13a
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-05 13:55:34 +0200

    Home component in the Route component is updated.

--- src/frontend/movie-gold-v1/src/App.js
+++ src/frontend/movie-gold-v1/src/App.js
@@ -35,7 +35,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Home/>}></Route>
+          <Route path="/" element={<Home movies = {movies}/>}></Route>
         </Route>
       </Routes>
 
