2c98139759f8645208fa31389afd58b40240fede
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-21 09:25:38 +0200

    Fontawesome and bootstrap components are imported into the Header.js.

--- src/frontend/movie-gold-v1/src/components/header/Header.js
+++ src/frontend/movie-gold-v1/src/components/header/Header.js
@@ -1,4 +1,10 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
