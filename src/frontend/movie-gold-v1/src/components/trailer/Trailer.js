9e0d94eb74bc0edd5302bc840a53dc049ca058c8
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-22 13:03:59 +0200

    Imported reactPlayer components and youtubeId of movie-trailers.

--- src/frontend/movie-gold-v1/src/components/trailer/Trailer.js
+++ src/frontend/movie-gold-v1/src/components/trailer/Trailer.js
@@ -1,1 +1,19 @@
 import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import './Trailer.css'
+
+import React from "react";
+
+const Trailer = () => {
+
+    let params = useParams();
+    const key = params.ytTrailerId;
+
+    return (
+        <div className="react-player-container">
+            
+        </div>
+    )
+}
+
+export default Trailer
