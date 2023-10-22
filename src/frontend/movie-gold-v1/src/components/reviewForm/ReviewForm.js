a59e5f429a49fe0cf4b0eaa487dc1158f8683601
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-22 13:45:16 +0200

    Added functionality into ReviewForm.js.

--- src/frontend/movie-gold-v1/src/components/reviewForm/ReviewForm.js
+++ src/frontend/movie-gold-v1/src/components/reviewForm/ReviewForm.js
@@ -1,10 +1,14 @@
-import React from "react";
+import {Form,Button} from 'react-bootstrap';
 
-const ReviewForm = () => {
+const ReviewForm = ({handleSubmit, revText, labelText, defaultValue}) => {
     return (
-        <div>
-
-        </div>
+        <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>{labelText}</Form.Label>
+                <Form.Control ref={revText} as="Textarea" rows={3} defaultValue={defaultValue} />
+            </Form.Group>
+            <Button variant="outline-info" onClick={handleSubmit}>Submit</Button>
+        </Form>
     )
 }
 
