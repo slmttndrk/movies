11c9fc2e2f91b42124a6a73c442302d802d7f7a6
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-22 13:50:48 +0200

    Created Reviews component as the parent component of ReviewForm.

---
+++ src/frontend/movie-gold-v1/src/components/reviews/Reviews.js
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from "react";
+import api from '../../api/axiosConfig';
+import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import ReviewForm from "../reviewForm/ReviewForm";
+
+import React from "react";
+
+const Reviews = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Reviews
