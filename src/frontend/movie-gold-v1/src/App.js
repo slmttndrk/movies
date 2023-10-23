5eebb456a4f1e572f1c464da1f2501451f2cb98a
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-23 09:10:26 +0200

    Reviews component added to the Route paramater.

--- src/frontend/movie-gold-v1/src/App.js
+++ src/frontend/movie-gold-v1/src/App.js
@@ -6,10 +6,13 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
 
   const getMovies = async () =>{
     try 
@@ -20,6 +23,25 @@ function App() {
       console.log(response.data);
 
       setMovies(response.data);
+
+    } 
+    catch (error) 
+    {
+      console.log(error);
+    }
+  }
+
+  const getMovieData = async (movieId) =>{
+    try 
+    {
+
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviews);
 
     } 
     catch (error) 
@@ -39,6 +61,7 @@ function App() {
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies = {movies}/>}></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} reviews={reviews} setReviews={setReviews}/>}></Route>
         </Route>
       </Routes>
 
