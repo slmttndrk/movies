e7a55327f51cc59576d34687f02947e34a9b5485
Selamettin Dirik <selamettinn.95@gmail.com>
2023-10-23 09:18:45 +0200

    Reviews component added into the Hero.js.

--- src/frontend/movie-gold-v1/src/App.js
+++ src/frontend/movie-gold-v1/src/App.js
@@ -61,7 +61,7 @@ function App() {
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies = {movies}/>}></Route>
           <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
-          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} reviews={reviews} setReviews={setReviews}/>}></Route>
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews}/>}></Route>
         </Route>
       </Routes>
 
--- src/frontend/movie-gold-v1/src/components/hero/Hero.js
+++ src/frontend/movie-gold-v1/src/components/hero/Hero.js
@@ -3,9 +3,18 @@ import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const Hero = ({movies}) => {
+
+    const navigate = useNavigate();
+
+    function reviews(movieId)
+    {
+        navigate(`/Reviews/${movieId}`);
+    }
+
     return (
         <div className='movie-carousel-container'>
             <Carousel>
@@ -30,6 +39,9 @@ const Hero = ({movies}) => {
                                                 />
                                             </div>
                                             </Link>
+                                            <div className="movie-review-button-container">
+                                                <Button variant="info" onClick={() => reviews(movie.ImdbId)}>Reviews</Button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
